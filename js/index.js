96e821206a8874f77b9fc85ccd7517691c8eb3e7
HildegarnFZ <84681181+Vicioware@users.noreply.github.com>
2026-01-21 01:59:01 -0300

    s

--- js/index.js
+++ js/index.js
@@ -973,8 +973,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     { text: 'Parte 6', url: 'https://www.mediafire.com/file/texn0g6427vcmza/Middl4_4arth_Shad8w_8f_M8rd8r.part6.rar/file' }
                 ]
             },
-            { text: 'Descargar parche', url: 'https://d2.consoletarget.com/?y=798093de&x=Y9IUMSpqEb8axjvBtdSbBbZ7PJ49es%2B0FpGvEVgGb6g97AmcrY%2FMO9H%2F3AhguMj4P0xCn%2B6tJeJGLI7GLWreiWm2AUUufANiGX15AFWVaqe4WsWmOMg8iiEnDaThhLXw', readMoreText: 'El juego estará en inglés luego de aplicar el crack que viene con el juego. Para solucionar esto, descargar el parche, aplicarlo reemplazando los archivos originales, y asegurarte de que el parámetro "Language" del archivo "steam_api.ini" esté en "spanish", de manera que se vea así:\n"Language=spanish" sin las comillas.\n\nMuy probablemente el parche sea marcado como virus o amenaza por tu navegador o tu antivirus. Asegúrate de permitir el archivo en tu sistema, no es dañino.' }
+            { text: 'Descargar parche', url: 'https://d2.consoletarget.com/?y=798093de&x=Y9IUMSpqEb8axjvBtdSbBbZ7PJ49es%2B0FpGvEVgGb6g97AmcrY%2FMO9H%2F3AhguMj4P0xCn%2B6tJeJGLI7GLWreiWm2AUUufANiGX15AFWVaqe4WsWmOMg8iiEnDaThhLXw', readMoreText: 'El juego estará en inglés luego de aplicar el crack que viene con el juego. Para solucionar esto, descarga el parche, aplícalo reemplazando los archivos originales, y asegurarte de que el parámetro "Language" del archivo "steam_api.ini" esté en "spanish", de manera que se vea así:\n"Language=spanish" sin las comillas.\n\nMuy probablemente el parche sea marcado como virus o amenaza por tu navegador o tu antivirus. Asegúrate de permitir el archivo en tu sistema, no es dañino.' }
         ]
+
 
 
 
